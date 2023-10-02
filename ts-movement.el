19b6da9e40bae0792380b61f2f78584dd944f60f
Harit Kapadia <haritkapadia@outlook.com>
2023-10-02 00:17:25 +0000

    Add multiple-cursors support

--- ts-movement.el
+++ ts-movement.el
@@ -21,16 +21,22 @@
   "Find any overlay in tsm/-overlays containing POINT."
   (seq-find (lambda (o) (gethash o tsm/-overlays)) (overlays-at point)))
 
+(defun tsm/-overlay-at-node (node)
+  "Create overlay of NODE and add to `tsm/-overlays'"
+  (let ((overlay (make-overlay (treesit-node-start node) (treesit-node-end node))))
+    (overlay-put overlay 'face 'lazy-highlight)
+    (overlay-put overlay 'evaporate t)
+    (overlay-put overlay 'node node)
+    ; Emacs documentation says integer must be nonnegative, but -1 seems to work...
+    ; (i.e. puts it below multiple-cursors region overlay)
+    (overlay-put overlay 'priority '(nil . -1))
+    (puthash overlay overlay tsm/-overlays)
+    overlay))
+
 (defun tsm/-overlay-at-point (point)
   "Get overlay at POINT, or make one and add to `tsm/-overlays' if it does not exist."
   (or (tsm/-find-overlay-at-point point)
-      (let* ((node (treesit-node-on point point))
-             (overlay (make-overlay (treesit-node-start node) (treesit-node-end node))))
-        (overlay-put overlay 'face 'highlight)
-        (overlay-put overlay 'evaporate t)
-        (overlay-put overlay 'node node)
-        (puthash overlay overlay tsm/-overlays)
-        overlay)))
+      (tsm/-overlay-at-node (treesit-node-on point point))))
 
 (defun tsm/delete-overlay-at-point (point)
   "Delete node indicator at POINT."
@@ -39,6 +45,31 @@
     (when overlay
       (delete-overlay overlay)
       (remhash overlay tsm/-overlays))))
+
+(defun tsm/-go-to-closest-overlay (point key)
+  (let* ((overlays (hash-table-keys tsm/-overlays))
+         (target-overlay
+          (seq-reduce (lambda (min overlay)
+                        (if (< (funcall key overlay) (funcall key min)) overlay min))
+                      (cdr overlays)
+                      (car overlays))))
+    (goto-char (treesit-node-start (tsm/-get-node target-overlay)))))
+
+(defun tsm/backward-overlay (point)
+  "Move POINT backward to the closest node indicator."
+  (interactive "d")
+  (cl-flet ((key (overlay)
+              (let ((value (- point (treesit-node-start (tsm/-get-node overlay)))))
+                (if (> value 0) value 1.0e+INF))))
+    (tsm/-go-to-closest-overlay point #'key)))
+
+(defun tsm/forward-overlay (point)
+  "Move POINT forward to the closest node indicator."
+  (interactive "d")
+  (cl-flet ((key (overlay)
+              (let ((value (- (treesit-node-start (tsm/-get-node overlay)) point)))
+                (if (> value 0) value 1.0e+INF))))
+    (tsm/-go-to-closest-overlay point #'key)))
 
 (defun tsm/node-prev (point)
   "Go to previous sibling of node at POINT and go to start of node."
@@ -99,6 +130,50 @@
     (push-mark (treesit-node-end node) nil t)
     (goto-char (treesit-node-start node))))
 
+(defun tsm/node-children (point)
+  "Select all immediate children of node at POINT that are TYPE."
+  (interactive "d")
+  (let* ((overlay (tsm/-overlay-at-point (point)))
+         (node (overlay-get overlay 'node)))
+    (cl-loop for i from 0 below (treesit-node-child-count node)
+          for child = (treesit-node-child node i)
+          do (tsm/-overlay-at-node child))
+    (delete-overlay overlay)
+    (remhash overlay tsm/-overlays)))
+
+(defun tsm/-treesit-node-children (node)
+  "Get all children of NODE."
+  (cl-loop for i from 0 below (treesit-node-child-count node)
+        collect (treesit-node-child node i)))
+
+(defun tsm/-collect-types (list-of-nodes)
+  (delete-dups (mapcar #'treesit-node-type list-of-nodes)))
+
+(defun tsm/-get-node (overlay)
+  (overlay-get overlay 'node))
+
+(defun tsm/node-children-of-type (type)
+  "Select all immediate children of node at POINT that are TYPE."
+  (interactive
+   (list (completing-read "Type: " (tsm/-collect-types (tsm/-treesit-node-children (tsm/-get-node (tsm/-overlay-at-point (point))))))))
+  (let* ((overlay (tsm/-overlay-at-point (point)))
+         (node (tsm/-get-node overlay)))
+    (cl-loop for i from 0 below (treesit-node-child-count node)
+          for child = (treesit-node-child node i)
+          if (string= type (treesit-node-type child))
+          do (tsm/-overlay-at-node child))
+    (delete-overlay overlay)
+    (remhash overlay tsm/-overlays)))
+
+(defun tsm/clear-overlays-of-type (type)
+  "Remove all overlays of TYPE."
+  (interactive
+   (list (completing-read "Type: " (tsm/-collect-types (mapcar #'tsm/-get-node (hash-table-keys tsm/-overlays))))))
+  (dolist (overlay (hash-table-keys tsm/-overlays))
+    (when (string= type (treesit-node-type (tsm/-get-node overlay)))
+      (delete-overlay overlay)
+      (remhash overlay tsm/-overlays))))
+
 (defun tsm/clear-overlays (&optional beg end)
   "Remove all overlays. BEG and END are unused."
   (interactive)
@@ -107,29 +182,67 @@
     (delete-overlay overlay))
   (clrhash tsm/-overlays))
 
+(defun tsm/mc/mark-all-overlays (point)
+  "Add a multiple-cursor cursor at the start of each node overlay"
+  (interactive "d")
+  (cl-flet ((key (overlay)
+              (abs (- point (treesit-node-start (tsm/-get-node overlay))))))
+    (let* ((overlays (hash-table-keys tsm/-overlays))
+           (current-overlay (tsm/-overlay-at-point point))
+           (target-overlay
+            (or current-overlay
+                (seq-reduce (lambda (min overlay)
+                              (if (< (key overlay) (key min)) overlay min))
+                            (cdr overlays)
+                            (car overlays)))))
+      (maphash
+       (lambda (overlay _)
+         (ignore _)
+         (unless (eql overlay target-overlay)
+           (goto-char (treesit-node-start (tsm/-get-node overlay)))
+           (mc/create-fake-cursor-at-point)))
+       tsm/-overlays)
+      (goto-char (treesit-node-start (tsm/-get-node target-overlay)))
+      (if (> (mc/num-cursors) 1)
+          (multiple-cursors-mode 1)
+        (mc/disable-multiple-cursors-mode)))))
+
 (defvar ts-movement-map
   (let ((map (make-sparse-keymap "Tree Sitter Movement")))
     (cond ((require 'hydra nil 'noerror)
            (defhydra tsm/hydra ()
              "TS Movement"
              ("d" #'tsm/delete-overlay-at-point)
+             ("D" #'tsm/clear-overlays-of-type)
+             ("C-b" #'tsm/backward-overlay)
+             ("C-f" #'tsm/forward-overlay)
              ("b" #'tsm/node-prev)
              ("f" #'tsm/node-next)
              ("p" #'tsm/node-parent)
              ("n" #'tsm/node-child)
+             ("N" #'tsm/node-children)
+             ("s" #'tsm/node-children-of-type)
              ("a" #'tsm/node-start)
              ("e" #'tsm/node-end)
-             ("m" #'tsm/node-mark))
+             ("m" #'tsm/node-mark)
+             ("c" #'tsm/mc/mark-all-overlays))
+           (push 'tsm/hydra/tsm/mc/mark-all-overlays mc--default-cmds-to-run-once)
            (define-key map (kbd "C-c .") #'tsm/hydra/body))
           (t
            (define-key map (kbd "C-c . d") #'tsm/delete-overlay-at-point)
+           (define-key map (kbd "C-c . D") #'tsm/clear-overlays-of-type)
            (define-key map (kbd "C-c . b") #'tsm/node-prev)
+           (define-key map (kbd "C-c . C-b") #'tsm/backward-overlay)
+           (define-key map (kbd "C-c . C-f") #'tsm/forward-overlay)
            (define-key map (kbd "C-c . f") #'tsm/node-next)
            (define-key map (kbd "C-c . p") #'tsm/node-parent)
            (define-key map (kbd "C-c . n") #'tsm/node-child)
+           (define-key map (kbd "C-c . N") #'tsm/node-children)
+           (define-key map (kbd "C-c . s") #'tsm/node-children-of-type)
            (define-key map (kbd "C-c . a") #'tsm/node-start)
            (define-key map (kbd "C-c . e") #'tsm/node-end)
-           (define-key map (kbd "C-c . m") #'tsm/node-mark)))
+           (define-key map (kbd "C-c . m") #'tsm/node-mark)
+           (define-key map (kbd "C-c . c") #'tsm/mark-all-overlays)))
     map))
 
 ;;;###autoload
